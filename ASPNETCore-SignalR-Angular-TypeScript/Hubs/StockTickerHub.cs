a4abb296e4935266dde65994132d45e21e32a592
Nemi Chand Jat <ch.nemichand@live.com>
2018-05-31 11:45:26 +0530

    added Stock ticker hub

---
+++ ASPNETCore-SignalR-Angular-TypeScript/Hubs/StockTickerHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore_SignalR_Angular_TypeScript.Hubs
+{
+
+	/// <summary>
+	/// stock ticker hub 
+	/// </summary>
+    public class StockTickerHub : Hub
+    {
+
+    }
+}
---
+++ ASPNETCore-SignalR-Angular-TypeScript/Models/Stock.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore_SignalR_Angular_TypeScript.Models
+{
+	public class Stock
+	{
+		private decimal _price;
+
+		public string Symbol { get; set; }
+
+		public decimal DayOpen { get; private set; }
+
+		public decimal DayLow { get; private set; }
+
+		public decimal DayHigh { get; private set; }
+
+		public decimal LastChange { get; private set; }
+
+		public decimal Change
+		{
+			get
+			{
+				return Price - DayOpen;
+			}
+		}
+
+		public double PercentChange
+		{
+			get
+			{
+				return (double)Math.Round(Change / Price, 4);
+			}
+		}
+
+		public decimal Price
+		{
+			get
+			{
+				return _price;
+			}
+			set
+			{
+				if (_price == value)
+				{
+					return;
+				}
+
+				LastChange = value - _price;
+				_price = value;
+
+				if (DayOpen == 0)
+				{
+					DayOpen = _price;
+				}
+				if (_price < DayLow || DayLow == 0)
+				{
+					DayLow = _price;
+				}
+				if (_price > DayHigh)
+				{
+					DayHigh = _price;
+				}
+			}
+		}
+	}
+}
